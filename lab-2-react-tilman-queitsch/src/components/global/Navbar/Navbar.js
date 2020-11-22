f7c56f410d50be19d843b93d8891c2222648f338
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-11-22 13:48:01 -0800

    Added hamburger menu, tested responsiveness

--- lab-2-react-tilman-queitsch/src/components/global/Navbar/Navbar.js
+++ lab-2-react-tilman-queitsch/src/components/global/Navbar/Navbar.js
@@ -14,12 +14,12 @@ class Navbar extends React.Component{
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">React</h1>
                 <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'bsi-times' : 'bsi-bars'}>
+                    <i className={this.state.clicked ? 'bars-hidden' : 'bars-shown'}>
                         <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-justify" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
                         </svg>
                     </i>
-                    <i className={this.state.clicked ? 'bsi-bars' : 'bsi-times'}>
+                    <i className={this.state.clicked ? 'x-shown' : 'x-hidden'}>
                         <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-x-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                         </svg>
