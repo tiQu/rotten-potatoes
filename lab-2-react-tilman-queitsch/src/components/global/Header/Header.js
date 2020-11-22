91212120ff27954c65205c98630546888328cd6a
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-11-22 10:37:34 -0800

    Added navbar and header components

---
+++ lab-2-react-tilman-queitsch/src/components/global/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { FaSearch } from 'react-icons/fa';
+// import { FaShoppingCart } from 'react-icons/fa';
+
+class Header extends React.Component{
+    render(){
+        return (
+            <header>
+                <div>
+                    Header component
+                </div>
+            </header>
+        );
+    }
+  }
+
+export default Header;
---
+++ lab-2-react-tilman-queitsch/src/components/global/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return (
+            <nav>
+                <div>
+                    Navbar component
+                </div>
+            </nav>
+        );
+    }
+  }
+
+export default Navbar;
