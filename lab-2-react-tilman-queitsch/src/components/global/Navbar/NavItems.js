6dc46ec8425382f371ddef7c6edac5c986bb6a22
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-11-22 10:43:52 -0800

    Adding scaffholding for navbar

---
+++ lab-2-react-tilman-queitsch/src/components/global/Navbar/NavItems.js
@@ -0,0 +1,27 @@
+const NavItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Subsection 1',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Subsection 2',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Subsection 3',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Subsection 4',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
--- lab-2-react-tilman-queitsch/src/components/global/Navbar/Navbar.js
+++ lab-2-react-tilman-queitsch/src/components/global/Navbar/Navbar.js
@@ -3,10 +3,14 @@ import React from 'react';
 class Navbar extends React.Component{
     render(){
         return (
-            <nav>
-                <div>
-                    Navbar component
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">React</h1>
+                <div ClassName="menu-icon">
+
                 </div>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
             </nav>
         );
     }
