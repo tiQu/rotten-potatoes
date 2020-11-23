f5a5358346f350ed5044f31d0152bd050895a372
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-11-22 23:15:43 -0800

    Added filler for MovieCard

---
+++ lab-2-react-tilman-queitsch/src/components/global/Home/MovieCard/MovieCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MovieCard.scss';
+
+class MovieCard extends React.Component{
+    render(){
+        return (
+            <div></div>
+        );
+    }
+  }
+
+export default MovieCard;
