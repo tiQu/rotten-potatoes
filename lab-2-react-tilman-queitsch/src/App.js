692877c803704fab9ff5ad750bb776104ba03e29
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-11-22 22:54:45 -0800

    Starting on movie cart component

--- lab-2-react-tilman-queitsch/src/App.js
+++ lab-2-react-tilman-queitsch/src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from './components/global/Header/Header';
 import Footer from './components/global/Footer/Footer';
+import Body from './components/global/Body/Body';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
       <Footer />
     </div>
   );
---
+++ lab-2-react-tilman-queitsch/src/components/global/Body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Body.scss';
+
+class Body extends React.Component{
+    render(){
+        return (
+            <body>
+                I has a body
+            </body>
+        );
+    }
+  }
+
+export default Body;
