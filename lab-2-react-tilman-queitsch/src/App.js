e95ed5e6a82e54f64e0e2d3fc4cd41d4d951a110
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-11-22 22:07:33 -0800

    More footer updates

--- lab-2-react-tilman-queitsch/src/App.js
+++ lab-2-react-tilman-queitsch/src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/global/Header/Header';
+import Footer from './components/global/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
